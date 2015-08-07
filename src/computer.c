df3723b0e66a5578b9165fc7c6aa7801ead5962e
Martin Kunev <martinkunev@gmail.com>
2015-08-07 18:27:12 +0300

    AI estimates damage done by enemies

--- src/computer.c
+++ src/computer.c
@@ -57,9 +57,9 @@ static double unit_importance(const struct battle *restrict battle, const struct
 	//
 }
 
-static unsigned pawn_victims(const struct game *restrict game, const struct battle *restrict battle, const struct pawn *restrict pawn, const struct pawn_command *restrict command, struct pawn *restrict victims[static 4])
+static unsigned pawn_victims(const struct game *restrict game, const struct pawn *pawns[BATTLEFIELD_HEIGHT][BATTLEFIELD_WIDTH], const struct pawn *restrict pawn, const struct pawn_command *restrict command, const struct pawn *restrict victims[static 4])
 {
-	struct pawn *victim;
+	const struct pawn *victim;
 	unsigned victims_count = 0;
 
 	struct point destination = command->moves[command->moves_count - 1].location;
@@ -69,7 +69,7 @@ static unsigned pawn_victims(const struct game *restrict game, const struct batt
 	// If the pawn has a specific fight target and is able to fight it, fight only that target.
 	if ((command->action == PAWN_FIGHT) && battlefield_neighbors(destination, command->target))
 	{
-		victims[victims_count++] = battle->field[command->target.y][command->target.x].pawn;
+		victims[victims_count++] = pawns[command->target.y][command->target.x];
 	}
 	else
 	{
@@ -77,13 +77,13 @@ static unsigned pawn_victims(const struct game *restrict game, const struct batt
 		int y = destination.y;
 
 		// Look for pawns to fight at the neighboring fields.
-		if ((x > 0) && (victim = battle->field[y][x - 1].pawn) && !allies(game, pawn->troop->owner, victim->troop->owner))
+		if ((x > 0) && (victim = pawns[y][x - 1]) && !allies(game, pawn->troop->owner, victim->troop->owner))
 			victims[victims_count++] = victim;
-		if ((x < (BATTLEFIELD_WIDTH - 1)) && (victim = battle->field[y][x + 1].pawn) && !allies(game, pawn->troop->owner, victim->troop->owner))
+		if ((x < (BATTLEFIELD_WIDTH - 1)) && (victim = pawns[y][x + 1]) && !allies(game, pawn->troop->owner, victim->troop->owner))
 			victims[victims_count++] = victim;
-		if ((y > 0) && (victim = battle->field[y - 1][x].pawn) && !allies(game, pawn->troop->owner, victim->troop->owner))
+		if ((y > 0) && (victim = pawns[y - 1][x]) && !allies(game, pawn->troop->owner, victim->troop->owner))
 			victims[victims_count++] = victim;
-		if ((y < (BATTLEFIELD_HEIGHT - 1)) && (victim = battle->field[y + 1][x].pawn) && !allies(game, pawn->troop->owner, victim->troop->owner))
+		if ((y < (BATTLEFIELD_HEIGHT - 1)) && (victim = pawns[y + 1][x]) && !allies(game, pawn->troop->owner, victim->troop->owner))
 			victims[victims_count++] = victim;
 	}
 
@@ -91,9 +91,7 @@ static unsigned pawn_victims(const struct game *restrict game, const struct batt
 }
 
 /*
-damage that the pawn can do is added (more if it's done to stronger units)
-damage that is done to the pawn is subtracted in the same manner
-possibility of shooting, assault and attack
+damage that is done to the pawn is subtracted (more if it's done by stronger units)
 closeness to allies
 */
 static double state_assess(const struct game *restrict game, struct battle *restrict battle, unsigned char player, const struct pawn_command *restrict commands)
@@ -104,21 +102,28 @@ static double state_assess(const struct game *restrict game, struct battle *rest
 
 	double rating = 0.0;
 
+	const struct pawn *victims[4];
+	unsigned victims_count;
+
+	struct pawn_command *command = malloc(offsetof(struct pawn_command, moves) + sizeof(*command->moves));
+
 	// WARNING: We assume the locations where the pawns are commanded to go will be non-occupied.
 
 	// Estimate which pawn will occupy a given location after the move.
-	for(i = 0; i < battle->players[i].pawns_count; ++i) // loop the pawns the player controls
+	for(i = 0; i < battle->pawns_count; ++i)
 	{
-		const struct pawn *restrict pawn = battle->players[i].pawns[i];
+		const struct pawn *restrict pawn = battle->pawns + i;
 		struct point location;
 
 		if (!pawn->count) continue;
 
-		location = commands[i].moves[commands[i].moves_count - 1].location;
+		if (pawn->troop->owner == player) location = commands[i].moves[commands[i].moves_count - 1].location;
+		else location = pawn->moves[0].location; // TODO do better estimation here
 		pawns_moved[location.y][location.x] = pawn;
 	}
 
-	for(i = 0; i < battle->players[i].pawns_count; ++i) // loop the pawns the player controls
+	// Estimate how beneficial is the command given to each of the player's pawns.
+	for(i = 0; i < battle->players[player].pawns_count; ++i) // loop the pawns the player controls
 	{
 		const struct pawn *restrict pawn = battle->players[i].pawns[i];
 		const struct pawn_command *restrict command = commands + i;
@@ -128,41 +133,49 @@ static double state_assess(const struct game *restrict game, struct battle *rest
 		if (command->action == PAWN_SHOOT)
 		{
 			// estimate shoot impact
+			// TODO
 		}
 		else if ((command->action == PAWN_ASSAULT) && battlefield_neighbors(command->moves[command->moves_count - 1].location, command->target))
 		{
 			// estimate assault impact
+			// TODO
 		}
 		else
 		{
 			// estimate fight impact
 
-			struct pawn *victims[4];
-			unsigned victims_count = pawn_victims(game, battle, pawn, command, victims);
+			victims_count = pawn_victims(game, pawns_moved, pawn, command, victims);
 
 			for(j = 0; j < victims_count; ++j)
 				rating += damage_expected(pawn, (double)pawn->count / victims_count, victims[j]) * unit_importance(battle, victims[j]->troop->unit);
 		}
+
+		// TODO increase rating on future possibility of shooting, assault and attack
+
+		// TODO walls close to the current position (if on the two diagonals, large bonus (gate blocking))
 	}
-	for(i = 0; i < battle->pawns_count; ++i) // loop ally and enemy pawns
+
+	// TODO take ally pawns into account
+
+	// Estimate how bad will the damage effects from enemies be.
+	for(i = 0; i < battle->pawns_count; ++i) // loop enemy pawns
 	{
 		const struct pawn *restrict pawn = battle->pawns + i;
-		//struct point location;
 
 		if (!pawn->count) continue;
-		if (pawn->troop->owner == player) continue;
+		if (allies(game, pawn->troop->owner, player)) continue;
 
-		// location = pawn->moves[pawn->moves_count - 1].location;
-		// battle->field[location.y][location.x]
+		// TODO guess when a pawn will prefer shoot/assault or fighting specific target
+		command->action = 0;
+		command->moves_count = 1;
+		command->moves[0] = pawn->moves[0];
+
+		victims_count = pawn_victims(game, pawns_moved, pawn, command, victims);
+		for(j = 0; j < victims_count; ++j)
+			rating -= damage_expected(pawn, (double)pawn->count / victims_count, victims[j]) * unit_importance(battle, victims[j]->troop->unit);
 	}
 
-	/*
-	if (allies(game, pawn->troop->owner, player))
-		;
-	else
-		;
-	// TODO walls close to the current position (if on the two diagonals, large bonus (gate blocking))
-	*/
+	free(command);
 
 	return 1.0;
 }
